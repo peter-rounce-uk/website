42487a6623819dbeea067c871548d55ba089ed53
Peter Rounce <peter@rounce.me.uk>
2012-12-26 23:16:50 +0000

    try Jekyll

---
+++ assets/js/all.js
@@ -0,0 +1,16 @@
+---
+---
+
+{% include jquery.js %}
+{% include bootstrap-transition.js %}
+{% include bootstrap-alert.js %}
+{% include bootstrap-modal.js %}
+{% include bootstrap-dropdown.js %}
+{% include bootstrap-scrollspy.js %}
+{% include bootstrap-tab.js %}
+{% include bootstrap-tooltip.js %}
+{% include bootstrap-popover.js %}
+{% include bootstrap-button.js %}
+{% include bootstrap-collapse.js %}
+{% include bootstrap-carousel.js %}
+{% include bootstrap-typeahead.js %}
